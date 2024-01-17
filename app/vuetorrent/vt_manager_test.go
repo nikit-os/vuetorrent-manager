f5209fb827a5cc7f6455a06d6e3fc5966d7789bb
n1kit-os <nik.scrp@gmail.com>
2024-01-17 21:34:16 +0200

    add tests for retrieving releases

--- app/vuetorrent/vt_manager_test.go
+++ app/vuetorrent/vt_manager_test.go
@@ -1,6 +1,7 @@
 package vuetorrent
 
 import (
+	"n1kit0s/vt-manager/app/github"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -8,6 +9,73 @@ import (
 	"strings"
 	"testing"
 )
+
+type mockGithubClient struct {
+}
+
+func (c *mockGithubClient) GetReleases() ([]github.Release, error) {
+	return []github.Release{
+		{TagName: "v1.1.1", Assets: []github.Asset{{Name: "vuetorrent.zip", DownloadUrl: "http://localhost:9876/dw/vuetorrent-111.zip"}}},
+		{TagName: "v1.1.2", Assets: []github.Asset{{Name: "vuetorrent.zip", DownloadUrl: "http://localhost:9876/dw/vuetorrent-112.zip"}}},
+		{TagName: "v1.1.3", Assets: []github.Asset{{Name: "vuetorrent.zip", DownloadUrl: "http://localhost:9876/dw/vuetorrent-113.zip"}}},
+	}, nil
+}
+func (c *mockGithubClient) GetReleaseByTag(tag string) (github.Release, error) {
+	return github.Release{
+		TagName: tag, Assets: []github.Asset{{Name: "vuetorrent.zip", DownloadUrl: "http://localhost:9876/dw/vuetorrent.zip"}},
+	}, nil
+}
+
+func TestGetAllReleases(t *testing.T) {
+	// Setup
+	githubClient := &mockGithubClient{}
+	vtManager := NewVTManager(githubClient)
+	expectedReleases := []VueTorrentRelease{
+		{Version: "1.1.1", DownloadUrl: "http://localhost:9876/dw/vuetorrent-111.zip"},
+		{Version: "1.1.2", DownloadUrl: "http://localhost:9876/dw/vuetorrent-112.zip"},
+		{Version: "1.1.3", DownloadUrl: "http://localhost:9876/dw/vuetorrent-113.zip"},
+	}
+
+	// Run
+	releases, _ := vtManager.GetAllReleases()
+	for i, release := range releases {
+		if release != expectedReleases[i] {
+			t.Errorf("Actual: %+v | Expected: %+v", release, expectedReleases[i])
+		}
+	}
+}
+
+func TestGetLatestVuetorrentRelease(t *testing.T) {
+	// Setup
+	githubClient := &mockGithubClient{}
+	vtManager := NewVTManager(githubClient)
+	expectedRelease := VueTorrentRelease{
+		Version:     "1.1.1",
+		DownloadUrl: "http://localhost:9876/dw/vuetorrent-111.zip",
+	}
+
+	// Run
+	release, _ := vtManager.GetLatestVuetorrentRelease()
+	if release != expectedRelease {
+		t.Errorf("Actual: %+v | Expected: %+v", release, expectedRelease)
+	}
+}
+
+func TestGetVuetorrentRelease(t *testing.T) {
+	// Setu
+	githubClient := &mockGithubClient{}
+	vtManager := NewVTManager(githubClient)
+	expectedRelease := VueTorrentRelease{
+		Version:     "1.1.2",
+		DownloadUrl: "http://localhost:9876/dw/vuetorrent.zip",
+	}
+
+	// Run
+	release, _ := vtManager.GetVuetorrentRelease("1.1.2")
+	if release != expectedRelease {
+		t.Errorf("Actual: %+v | Expected: %+v", release, expectedRelease)
+	}
+}
 
 func TestUnzip(t *testing.T) {
 	// Setup
@@ -47,7 +115,7 @@ func TestDownload(t *testing.T) {
 	defer server.Close()
 
 	vtRelease := VueTorrentRelease{
-		Version: "1.2",
+		Version:     "1.2",
 		DownloadUrl: server.URL,
 	}
 	outputDir := t.TempDir()
@@ -72,8 +140,8 @@ func TestDownload(t *testing.T) {
 func TestDownloadShouldSkipIfFileExists(t *testing.T) {
 	// Setup
 	vtRelease := VueTorrentRelease{
-		Version: "1.2",
-		DownloadUrl: "shoul not be used",
+		Version:     "1.2",
+		DownloadUrl: "should not be used",
 	}
 	outputDir := t.TempDir()
 	expectedFilename := "vuetorrent-1.2.zip"
@@ -84,6 +152,5 @@ func TestDownloadShouldSkipIfFileExists(t *testing.T) {
 	if err != nil {
 		t.Fatalf("download failed. error: %s", err.Error())
 	}
-
 
 }
