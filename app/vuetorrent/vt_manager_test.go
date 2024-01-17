e5bd8c87526cdc88d64e9e2c673af661a60ea5c2
n1kit-os <nik.scrp@gmail.com>
2024-01-17 20:45:21 +0200

    add tests for download function

--- app/vuetorrent/vt_manager_test.go
+++ app/vuetorrent/vt_manager_test.go
@@ -1,6 +1,8 @@
 package vuetorrent
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"strings"
@@ -8,6 +10,7 @@ import (
 )
 
 func TestUnzip(t *testing.T) {
+	// Setup
 	files := []TestFile{
 		{Path: "vuetorrent/version.txt", Content: "1.2.3"},
 		{Path: "vuetorrent/folder1/file1.js", Content: "file1 content"},
@@ -21,6 +24,7 @@ func TestUnzip(t *testing.T) {
 
 	outputDir := filepath.Join(t.TempDir(), "test_out")
 
+	// Run
 	err := unzip(archivePath, outputDir, "0.0.0")
 	if err != nil {
 		t.Fatalf("Failed to unzip. Error: %s", err.Error())
@@ -33,5 +37,53 @@ func TestUnzip(t *testing.T) {
 			t.Errorf("File [%s] was not unarchived into [%s]", expectedFilePath, outputDir)
 		}
 	}
+}
+
+func TestDownload(t *testing.T) {
+	// Setup
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("file content"))
+	}))
+	defer server.Close()
+
+	vtRelease := VueTorrentRelease{
+		Version: "1.2",
+		DownloadUrl: server.URL,
+	}
+	outputDir := t.TempDir()
+
+	// Run
+	downloadedFilePath, err := download(vtRelease, outputDir)
+	if err != nil {
+		t.Fatalf("download failed. error: %s", err.Error())
+	}
+
+	fileInfo, err := os.Stat(downloadedFilePath)
+	if err != nil {
+		t.Fatalf("File was not downloaded into [%s]. Error: %s", outputDir, err.Error())
+	}
+
+	expectedFilename := "vuetorrent-1.2.zip"
+	if fileInfo.Name() != expectedFilename {
+		t.Fatalf("Expected filename is [%s]. Actual filename is [%s]", expectedFilename, fileInfo.Name())
+	}
+}
+
+func TestDownloadShouldSkipIfFileExists(t *testing.T) {
+	// Setup
+	vtRelease := VueTorrentRelease{
+		Version: "1.2",
+		DownloadUrl: "shoul not be used",
+	}
+	outputDir := t.TempDir()
+	expectedFilename := "vuetorrent-1.2.zip"
+	os.Create(filepath.Join(outputDir, expectedFilename))
+
+	// Run
+	_, err := download(vtRelease, outputDir)
+	if err != nil {
+		t.Fatalf("download failed. error: %s", err.Error())
+	}
+
 
 }
